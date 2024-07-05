de47cf05133d4a94fec53f99f6cad1da045c7318
Евгений Билан <evgenebilan@MacBook-Air-Evgenij.local>
2024-07-05 23:32:02 +0300

    fix: fix  registration form

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -1,35 +1,53 @@
+"use client"
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [companyInfo, setCompanyInfo] = useState("");
+
+  const handleSubmit = () => {
+
+    console.log(email, password, companyInfo);
+    axios
+      .post("http://localhost:4000/auth/signup", {
+        email,
+        password,
+        companyInfo,
+      })
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        window.location.href = "/dashboard/posts";
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
       <h1 className="text-3xl font-bold">Register</h1>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <div className="space-y-6">
           <div>
             <label
-              htmlFor="email"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Email address
             </label>
             <div className="mt-2">
               <input
-                id="email"
-                name="email"
                 type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 autoComplete="email"
                 required
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-
           <div>
             <div className="flex items-center justify-between">
               <label
-                htmlFor="password"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Password
@@ -37,28 +55,27 @@ const RegisterPage = () => {
             </div>
             <div className="mt-2">
               <input
-                id="password"
-                name="password"
                 type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 autoComplete="current-password"
                 required
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
-
           <div>
             <label
-              htmlFor="companyInfo"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
               Company info
             </label>
             <div className="mt-2">
               <input
-                id="companyInfo"
-                name="companyInfo"
                 type="text"
+                value={companyInfo}
+                onChange={(e) => setCompanyInfo(e.target.value)}
+                autoComplete="companyInfo"
                 required
                 className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -66,14 +83,14 @@ const RegisterPage = () => {
           </div>
 
           <div>
-            <button
-              type="submit"
+          <button
+              onClick={handleSubmit}
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Sign up
             </button>
           </div>
-        </form>
+        </div>
 
         <p className="mt-10 text-center text-sm text-gray-500">
           Already have account?{" "}
